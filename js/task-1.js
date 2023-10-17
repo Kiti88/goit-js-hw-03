767a8ebd5ad86462c3297b6dd7920dd27236a708
Kiti88 <a3kotik@gmail.com>
2023-10-17 22:17:20 +0300

    2217

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,5 @@
 ffunction slugify(title) {
+
   // Перетворення на нижній регістр та розділення на слова
   const words = title.toLowerCase().split(" ");
   
@@ -12,4 +13,5 @@ ffunction slugify(title) {
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+// "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 function filterArray(numbers, value) {
+
   const result = [];
   for (let i = 0; i < numbers.length; i++) {
+
     if (numbers[i] > value) {
+      
       result.push(numbers[i]);
     }
   }
